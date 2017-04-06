974babeb94cc1a9a61f107f6cd80c16562e5749e
cheyuwu <frank890417@gmail.com>
2017-04-06 14:22:12 +0800

    add lang dropdown

--- resources/assets/js/components/Navbar.vue
+++ resources/assets/js/components/Navbar.vue
@@ -30,13 +30,27 @@ nav.navbar.navbar-default.navbar-fixed-top.at_top
             router-link(to="/index") 會員服務
 
         ul.nav.navbar-nav.navbar-right
-          li 繁
-            i.fa.fa-angle-down 
+          li.dropdown.lang_dropdown
+            a.dropdown-toggle(href="#" data-toggle="dropdown" role="button" aria-expanded="false")
+              span 繁
+              i.fa.fa-angle-down 
+              
+            ul.dropdown-menu(role="menu")
+              li
+                a(href="#") 繁
+              li
+                a(href="#") 简
+              li
+                a(href="#") EN
+            
+
           li 
             i.fa.fa-search
           li
             .icon_text_size.bigger 
-              .text A+
+              .text A
+                span.plus +
+
 
           li.nav_open 
             i.fa.fa-bars
