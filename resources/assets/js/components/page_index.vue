58c22da0a6f44fab8b1b6724112de394d8aa5ebb
cheyuwu <frank890417@gmail.com>
2017-04-13 14:41:52 +0800

    Update index number data

--- resources/assets/js/components/page_index.vue
+++ resources/assets/js/components/page_index.vue
@@ -41,7 +41,7 @@ div.page_index
       .col_right
         h3.section_title 孩子成長的生活環境
         p.section_para.text-left 我們的生活環境有許多食安風險與汙染問題直接影響健康，這些有毒的化學物質被不肖業者濫用，使得我們的飲食與環境到處充斥具有危害與累積性毒素。<br><br>全世界每天有約5,500位兒童死於污染的水、空氣與食物所導致的疾病。
-        .percent.text-right(data-target=75) 5500
+        .percent.text-right(data-target=5500) 5500
 
   section.page_index_live
     .container.flex
@@ -49,7 +49,7 @@ div.page_index
         h3.section_title 從居家到工作環境
         p.section_para.text-left 近年來食安問題層出不窮，引發全民對食品安全的恐慌與疑慮。有鑒於食安事件中不乏多家知名大廠，凸顯了業者自主管理的漏洞。食品安全須從源頭管理做起，以保障民眾得到安全的食物來源。<br><br>根據估計，每年食安事件導致的死亡人數高達200萬人。
         router-link.btn.btn-primary(to='/tech') 了解更多
-        .percent.text-right(data-target=42) 200
+        .percent.text-right(data-target=200) 200
       .col_right
         
 
