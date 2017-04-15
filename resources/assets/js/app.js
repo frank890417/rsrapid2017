4019d27fff2bd4d7f5dfc8c1877a234817d10b1a
cheyuwu <frank890417@gmail.com>
2017-04-15 09:52:06 +0800

    Issue fix not found mountain

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -212,7 +212,9 @@ scroll
 //使用卷軸位置更新元件
 window.update_scroll=function update_scroll(top_val){
   $(".bg_parallax").css("background-position","center "+top_val/5.00+"px");
-  $(".mountain").css("bottom",(+(-(top_val+window_height*0.75-$("#section_about_log").offset().top)/4))+"px");
+  if ($(".mountain").length>0){
+    $(".mountain").css("bottom",(+(-(top_val+window_height*0.75-$("#section_about_log").offset().top)/4))+"px");
+  }
 
   //percet nt init
   $(".percent.initial").each(function(index,obj){
