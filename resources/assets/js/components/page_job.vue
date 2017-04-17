7755c2dccc4e8c80109764337197f08ef7a782d1
cheyuwu <frank890417@gmail.com>
2017-04-17 09:29:42 +0800

    Update work page

--- resources/assets/js/components/page_job.vue
+++ resources/assets/js/components/page_job.vue
@@ -10,23 +10,20 @@ div.page_job
     .container.flex.row.wrap
       .job_box
         .job_box_inner
-          h3.title 檢驗研發處 工程師
+          h4.title 工程師-1
             i.fa.fa-link
           p 需求人數: 2 人 | 3~5 年以上經驗
-          p 1. 熟悉LC / MS /MS儀器操作<br>2. 據檢測實驗室研發工作經驗者佳<br>3. 有儀器分析或食品檢測領域工作經驗<br> 地點：深圳
+          .cover.flex.column 
+            p 1. 熟悉LC / MS /MS儀器操作 (分析方法確效及分析方法開發)<br>2. 有儀器分析或食品檢測領域工作經驗
+            a.btn.btn-transparent(href="https://www.104.com.tw/job/?jobno=4s08f&jobsource=n104bank1") 更多資訊
       .job_box
         .job_box_inner
-          h3.title 工程師-1
+          h4.title 工程師-2
             i.fa.fa-link
           p 需求人數: 2 人 | 3~5 年以上經驗
-          p 1. 熟悉LC / MS /MS儀器操作 (分析方法確效及分析方法開發)<br>2. 有儀器分析或食品檢測領域工作經驗
-      .job_box
-        .job_box_inner
-          h3.title 工程師-2
-            i.fa.fa-link
-          p 需求人數: 2 人 | 3~5 年以上經驗
-          p 1. 熟悉LC / MS /MS儀器操作 <br>2. 有儀器分析或食品檢測領域工作經驗
-          
+          .cover.flex.column 
+            p 1. 熟悉LC / MS /MS儀器操作 <br>2. 有儀器分析或食品檢測領域工作經驗
+            a.btn.btn-transparent(href="https://www.104.com.tw/job/?jobno=4s08g&jobsource=n104bank1") 更多資訊
       .row
         .col-sm-12
          
