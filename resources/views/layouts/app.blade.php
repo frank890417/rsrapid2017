66310dbc8819313c3d76557f7e930d4c6cc12236
cheyuwu <frank890417@gmail.com>
2017-07-02 22:32:40 +0800

    fix title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Zashare 雜學校</title>
+    <title>{{$metas["meta_title"]}}</title>
 
 
      <meta property="og:title" content="{{$metas["meta_title"]}}" />
