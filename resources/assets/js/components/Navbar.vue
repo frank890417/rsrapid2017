20b304730f113088def4d0a9f2dc3897fae7897d
cheyuwu <frank890417@gmail.com>
2017-07-07 13:20:05 +0800

    hide other lang

--- resources/assets/js/components/Navbar.vue
+++ resources/assets/js/components/Navbar.vue
@@ -96,7 +96,11 @@ div
             open_full: false,
             open_lang: false,
             nav_structure,
-            lang: [{name: "繁",link: "http://www.rapidsuretech.com/"},{name: "简",link: "http://cn.rapidsuretech.com/"},{name: "EN",link: "http://en.rapidsuretech.com/"}]
+            lang: [
+              {name: "繁",link: "http://www.rapidsuretech.com/"},
+              // {name: "简",link: "http://cn.rapidsuretech.com/"},
+              // {name: "EN",link: "http://en.rapidsuretech.com/"}
+            ]
           }
         },
         watch:{
